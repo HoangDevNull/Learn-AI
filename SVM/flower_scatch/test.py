ada497e9ae472f4c47cba837511e10d0cc3533a3
HoangPham <pqhoang.17it2@sict.udn.vn>
2019-11-28 09:15:36 +0700

    v2

--- SVM/flower_scatch/test.py
+++ SVM/flower_scatch/test.py
@@ -7,48 +7,51 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 import mahotas
 
-## Doc lai du global_features va labels tu file da luu
-## Chay mo hinh SVC
-
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import numpy as np
-import matplotlib.pyplot as plt
-import mahotas
 
 from cvxopt import matrix, solvers
 
 X = np.loadtxt("feature-datas.txt")
 y = np.loadtxt("label-datas.txt")
+
+
+model = SVC(kernel = 'linear', C = 1e5)
+model.fit(X, y)
+
+
 N = X.shape[0]
-y = y.reshape(-1,1) * 1.
-X_dash = y * X
-V = np.dot(X_dash , X_dash.T) * 1.
+V = X.T * y
 
+K = matrix(V.T.dot(V))
+K = matrix(K, K.size, 'd')
 
-
-K = matrix(V)
 p = matrix(-np.ones((N, 1)))
 G = matrix(-np.eye(N))
 h = matrix(np.zeros(N))
 A = matrix(y.reshape(1, -1))
-b = matrix(np.zeros(1))
+b = matrix(np.zeros(1)) 
 
 solvers.options['show_progress'] = False
 sol = solvers.qp(K, p, G, h, A, b)
 
 l = np.array(sol['x'])
+print(l.size, 'l size')
+# print('lambda = ', l)
 
-w = ((y * l).T @ X).reshape(-1,1)
 
-S = (l > 1e-4).flatten()
+epsilon = 1e-6
+S = np.where(l > epsilon)[0]
+VS = V.T[:, S]
+XS = X[:, S]
+y = np.matrix(y)
+yS = y[:, S]
+lS = l[S]
+# calculate w and b
+w = VS.dot(lS)
+b = np.mean(yS.T - w.T.dot(XS))
 
-b = y[S] - np.dot(X[S], w)
+print('w = ', w.T)
+print('b = ', b)
 
-print('lambda = ',l[l > 1e-4])
-print('w = ', w.flatten())
-print('b = ', b[0])
 
 from sklearn.preprocessing import MinMaxScaler
 bins = 8
@@ -87,7 +90,7 @@ import matplotlib.pyplot as plt
 
 fixed_size       = tuple((500, 500))
 
-image = cv2.imread('1.jpg')
+image = cv2.imread('2.jpg')
 
 # resize the image
 image = cv2.resize(image, fixed_size)
@@ -104,34 +107,29 @@ fv_histogram  = fd_histogram(image)
     ###################################
 global_feature = np.hstack([fv_histogram, fv_haralick, fv_hu_moments])
 
-# scale features in the range (0-1)
-scaler = MinMaxScaler(feature_range=(0, 1))
+# scale features in the range (-1-1)
+scaler = MinMaxScaler(feature_range=(-1, 1))
 rescaled_feature = scaler.fit_transform(global_feature.reshape(1, -1))
 
-
-# predict label of test image
 x = rescaled_feature.reshape(1,-1)
 
-print(x)
+print(x.shape, 'x shape ')
+print(w.shape[0], 'wshape')
 
-mypredict = (w[0] * x[0] + w[1] * x[0] + b) / np.sqrt((w[0] * w[0] + w[1]*w[1]))
+ws = 0
+xs = 0
 
-print('mypredict = ' , mypredict)
+for i in range(0, w.shape[0] -1): 
+    ws += (w[i] * w[i])
+    xs += (w[i] * x[0][i])
 
 
-# label = 'hoa 1'
+mypredict = (xs + b) / np.sqrt(ws)
+print(mypredict,'my predict')
 
-# if prediction==1.0:
-#     label ='tulip'
-# elif prediction==2.0:
-#     label ='windflower'
 
-# print (label)
- 
-
-# # show predicted label on image
-# cv2.putText(image, str(label), (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,255), 3)
-
-# # display the output image
-# plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-# plt.show()
+w1 = model.coef_
+b1 = model.intercept_
+print('w1 = ', w1)
+print('b1 = ', b1)
+print(model.predict(global_feature.reshape(1,-1))[0], 'svc')
